4ce7722ad5b17288f3e36a0f4c5d47f5caec3744
RedArms <reda_1030@hotmail.com>
2023-12-05 03:02:03 +0100

    change : 127.0.0.1 to 0.0.0.0

--- src/main.rs
+++ src/main.rs
@@ -29,7 +29,7 @@ async fn main() -> std::io::Result<()> {
             .service(hello)
             .service(get_item)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
