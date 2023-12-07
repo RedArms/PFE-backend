8345b10856d4b835792b1565b56775df0ec4d330
Brahim <brahim.abid@student.vinci.be>
2023-12-07 17:10:43 +0100

    Update bind address in main.rs

--- src/main.rs
+++ src/main.rs
@@ -77,7 +77,7 @@ async fn main() -> std::io::Result<()> {
             .service(index_route)
             
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
