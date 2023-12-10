5fd39dfecf062ce5eb05b59ff60c5ff6f097d511
Younes Benbouchta <younes.benbouchta@student.vinci.be>
2023-12-11 00:53:22 +0100

    Add client repository with CRUD operations

---
+++ src/repository/client_repository.rs
@@ -0,0 +1,87 @@
+use crate::models::client::Client;
+use crate::models::regular_order::RegularOrder;
+use crate::models::regular_order_line::RegularOrderLine;
+use crate::models::item::Item;
+use crate::routes::items;
+use sqlx::postgres::PgPool;
+use sqlx::Error;
+use actix_web::web;
+
+#[derive(Clone)]
+pub struct ClientRepository {
+    app_state: web::Data<crate::AppState>,
+}
+
+impl ClientRepository {
+    pub fn new(app_state: web::Data<crate::AppState>) -> Self {
+        Self { app_state }
+    }
+
+    pub async fn get_all_clients(&self) -> Result<Vec<Client>, Error> {
+        let client = sqlx::query_as!(Client, "SELECT client_id, name, address, tour FROM pfe.clients")
+            .fetch_all(&self.app_state.db_pool)
+            .await?;
+
+        Ok(client)
+    }
+
+    pub async fn add_client(&self, client: Client) -> Result<Client, Error> {
+        let client = sqlx::query_as!(Client, "INSERT INTO pfe.clients (name, address, tour) VALUES ($1, $2, $3) RETURNING client_id, name, address, tour", client.name, client.address, client.tour)
+            .fetch_one(&self.app_state.db_pool)
+            .await?;
+
+        let items: Vec<Item> = sqlx::query_as!(Item, "SELECT item_id, label, size FROM pfe.items")
+            .fetch_all(&self.app_state.db_pool)
+            .await?;
+
+        for item in items {
+            sqlx::query!("INSERT INTO pfe.client_lines (client, item, quantity) VALUES ($1, $2, 0)", client.client_id.unwrap(), item.item_id)
+                .execute(&self.app_state.db_pool)
+                .await?;
+        }
+    
+        Ok(client)
+    }
+
+    pub async fn get_order(&self, id: i32) -> Result<RegularOrder, Error> {
+        let regular_order = sqlx::query_as!(RegularOrderLine, "SELECT i.item_id, i.label, i.size, cl.quantity FROM pfe.client_lines cl JOIN pfe.items i ON cl.item = i.item_id WHERE cl.client = $1", id)
+            .fetch_all(&self.app_state.db_pool)
+            .await?;
+
+        Ok(RegularOrder::new(regular_order))
+    }
+
+    pub async fn update_order(&self, id: i32, order: RegularOrder) -> Result<(), Error> {
+        for line in order.regular_order_lines {
+            sqlx::query!("UPDATE pfe.client_lines SET quantity = $1 WHERE client = $2 AND item = $3", line.quantity, id, line.item_id)
+                .execute(&self.app_state.db_pool)
+                .await?;
+        }
+
+        Ok(())
+    }
+
+    pub async fn delete_client(&self, id: i32) -> Result<(), Error> {
+        let query = sqlx::query!("SELECT order_id FROM pfe.orders WHERE client = $1", id)
+            .fetch_one(&self.app_state.db_pool)
+            .await?;
+
+        sqlx::query!("DELETE FROM pfe.boxes WHERE order_id = $1", query.order_id)
+            .execute(&self.app_state.db_pool)
+            .await?;
+
+        sqlx::query!("DELETE FROM pfe.orders WHERE client = $1", id)
+            .execute(&self.app_state.db_pool)
+            .await?;
+
+        sqlx::query!("DELETE FROM pfe.client_lines WHERE client = $1", id)
+            .execute(&self.app_state.db_pool)
+            .await?;
+
+        sqlx::query!("DELETE FROM pfe.clients WHERE client_id = $1", id)
+            .execute(&self.app_state.db_pool)
+            .await?;
+
+        Ok(())
+    }
+}
