982cb3c6475cace80eba262b422b4d766ed14834
Younes Benbouchta <younes.benbouchta@student.vinci.be>
2023-12-11 00:52:50 +0100

    Add Client model to src/models/client.rs

---
+++ src/models/client.rs
@@ -0,0 +1,20 @@
+use serde::{Serialize, Deserialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Client {
+    pub client_id: Option<i32>,
+    pub name: String,
+    pub address: String,
+    pub tour: Option<i32>,
+}
+
+impl Client {
+    pub fn new(client_id: Option<i32>, name: &str, address: &str, tour: Option<i32>) -> Self {
+        Self {
+            client_id,
+            name: name.to_string(),
+            address: address.to_string(),
+            tour,
+        }
+    }
+}
